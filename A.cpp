b27e9e5a59aff8429dcf3e6553d82332ea968fdf
Shubham Singhal <singhalshubham89@gmail.com>
2020-04-10 20:18:56 +0530

    C++ debug and compile competitive coding

---
+++ A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int a;
+}
+
+int main()
+{
+    
+    int t;
+    
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+
+        cout << "Case #" << i << ": ";
+        solve();
+    }
+}
